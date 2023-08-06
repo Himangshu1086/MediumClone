3bbaa07a7f18cb29b08b53b63e2e8f5402eb4e64
Himangshu1086 <baishyahimangshu499@gmail.com>
2023-08-06 21:08:02 +0530

    update

--- clone_medium/src/components/home/home.js
+++ clone_medium/src/components/home/home.js
@@ -10,11 +10,11 @@ function Home() {
       <div className="bg-yellow-500" style={{borderBottom:'1px solid black'}}>
         <div className="flex justify-between ">
           <div className="sm:ml-60 ml-20 flex flex-col items-start justify-center ">
-            <h1 className="text-6xl" style={{fontSize:'80px'}}>Stay curious.</h1>
+            <h1 className="max-sm:leading-none" style={{fontSize:'80px'}}>Stay curious.</h1>
             <p className="m-4 text-3xl">Discover stories, thinking, and expertise from writers on any topic.</p>
             <button className="m-4 text-4xl rounded-3xl p-6 bg-black text-white hover:bg-blue-900" ><Link to='/write'>Write Article</Link></button>
           </div>
-          <div className="sm:flex hidden">
+          <div className="flex  max-sm:hidden">
             <img  src="https://images.unsplash.com/photo-1519337265831-281ec6cc8514?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"/>
           </div>
         </div>
